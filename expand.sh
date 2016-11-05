020a009683a1abbe06a9e23a0a58b8bdff76ffac
Josiah Hunsinger <jhunsinger2860@flploy.org>
2016-11-05 16:08:15 -0400

    initial commit

---
+++ expand.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DEV='/dev/sdb'
+PART='/dev/sdb2'
+
+# Get the total size of the SD card
+DISK_SIZE=$(fdisk -l $DEV | grep 'Disk /' | awk '{print $7}')
+
+# Get the start and size of the partition
+PART_START=$(fdisk -l $DEV | grep $PART | awk '{print $2}')
+PART_SIZE=$(fdisk -l $DEV | grep $PART | awk '{print $4}')
+
+# Calculate the new partion size that will make full use of the disk
+NEW_PART_SIZE=$(expr $DISK_SIZE - $PART_START)
+
+# Generate a human readable partition table file
+sfdisk -d $DEV > ./partition.layout
+
+# Replace occurences of the old partition size with the new partition size
+sed -i "s/$PART_SIZE/$NEW_PART_SIZE/g" ./partition.layout
+
+# Write the new partition table
+sfdisk $DEV < ./partition.layout
+rm ./partition.layout
+
+# Expand the filesystem to fill the newly sized partiotion
+resize2fs $PART
